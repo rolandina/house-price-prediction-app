869664537e67998f8707719a5d8ff93789b95740
Smirnova Nina <ms.nina.smirnova@gmail.com>
2021-07-23 18:21:41 +0200

    change data_analysis page

--- pages/data_analysis.py
+++ pages/data_analysis.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from model.data import Data
+import matplotlib.gridspec as gridspec
+import numpy as np
 
+sns.color_palette("bright")
+pal = sns.color_palette("bright").as_hex()
 
 def app():
     st.title('Data Analysis')
@@ -19,61 +24,173 @@ def app():
     train = data.get_prepared_train_data()
     target = "SalePrice"
 
-    plot_type = st.selectbox("Plot type:",("Numerical", "Categorical","Correlation-Resuduals"))
-
-    def create_list_numeric_columns(df):
-        return [column for column in df.columns if df[column].dtypes == "float" or (len(df[column].unique())>=15 and df[column].dtypes == "int")]
-
-    def create_list_categoric_columns(df):
-        return [column for column in df.columns if df[column].dtypes != "float" and df[column].dtypes != "int"] + [column for column in df.columns if df[column].dtypes == "int" and len(df[column].unique())<15]
+    empty, col1 = st.beta_columns([5,1])
+    with col1:
+        plot_type = st.selectbox("Plot type:",("Numerical", "Categorical","Correlation-Resuduals"))
 
 
-    def plot_numeric_features(df):
-        numerical_features = create_list_numeric_columns(df)
-        sns.set()  # Setting seaborn as default style even if use only matplotlib
-        sns.set_palette("Paired")  # set color palette
-        fig, axes = plt.subplots(nrows=len(numerical_features),
-                                ncols=2,
-                                figsize=(10, 3* len(numerical_features)))
-        for i, feature in enumerate(numerical_features):
-            sns.histplot(data=df, x=feature, kde=True, ax=axes[i, 0])
-            sns.boxplot(data=df, x=feature, ax=axes[i, 1])
-        st.pyplot(fig)
+    def plot_numeric_features(df, max_num_cat = 25):
+             
+        """Parameters:
+        plot_numerical_features(df, max_num_cat = 20)
+        df     - data frame with different types of features with target,
+        max_num_cat - number of classes in columns where the type is int by default = 25
+        
+        Output: display plots"""
+        
+        #condition for categorical features is the same as in plot_categoric_features()
+        cat_features = [column for column in df.columns if df[column].dtypes == "object" ] + \
+        [column for column in df.columns if df[column].dtypes == "int" and len(df[column].unique())< max_num_cat]
+        num_features = [col for col in df.columns if col not in cat_features]
+        
+        if len(num_features)>0:
+
+            # gridspec inside gridspec
+            num_plots = len(num_features)
+            #you can change number of columns
+            cols = 4 
+            rows = num_plots//cols if num_plots%cols == 0 else num_plots//cols +1
+
+            fig = plt.figure(figsize = (cols*5,rows*3))
+            gs0 = gridspec.GridSpec(rows, cols, figure=fig)
+            for i,feature in enumerate(num_features):
+                gs00 = gridspec.GridSpecFromSubplotSpec(5, 1, subplot_spec=gs0[i])
+
+                ax1 = fig.add_subplot(gs00[0, :])
+                ax2 = fig.add_subplot(gs00[1:-1, :])
+
+                sns.boxplot(data=df, x=feature, ax=ax1)
+                sns.histplot(data=df, x=feature, kde=True, ax=ax2)
+
+                #plt.subplots_adjust(wspace=0, hspace=0.1)
+                ax1.set(xlabel='')
+                ax1.tick_params(
+            axis='x',          # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            bottom=False,      # ticks along the bottom edge are off
+            top=False,         # ticks along the top edge are off
+            labelbottom=False) # labels along the bottom edge are off
+                ax1.axis('off')
+            st.pyplot(fig)
+                
+        else:
+            st.write('There are no numerical features in your dataset')
 
 
     def plot_categorical_features(df, target):  
-        categorical_features = create_list_categoric_columns(df)
+        """Parameters:
+        df - data frame with different types of features with target,
+        target - name of the column of dependent feature
+        Output: display plots"""
 
-        fig, axes = plt.subplots(nrows=len(categorical_features),
-                                ncols=1,
-                                figsize=(14, 4* len(categorical_features)))
-        for i, feature in enumerate(categorical_features):
-            
-            df_group = df[[feature, target]].groupby(feature).count()
-            #print(df_group)
-            sns.boxplot(data=df, x=feature, y= target, ax=axes[i])
-            sns.swarmplot(data=df, x=feature, y= target, ax=axes[i], color=".25", size = 2) #swarmplots
-            if len(df[feature].unique())>20:
-                plt.xticks(rotation=45)
-            axes[i].set_title(f"{target} by {feature}")
-        st.pyplot(fig)
+        # if target is categorical
+
+        #create df of categorical features
+        ## Attention on condition
+        case = 0  # target is numerical by default
+        cat_features = [
+            column for column in df.columns if df[column].dtypes == "object"
+        ] + [
+            column for column in df.columns
+            if df[column].dtypes == "int" and len(df[column].unique()) < 20
+        ]
+
+        if target in cat_features:
+            case = 1
+            cat_features.remove(target)
+
+        df_cat = df[cat_features]
+
+        if len(cat_features) > 0:
+
+            # gridspec inside gridspec
+            num_plots = len(cat_features)
+            #you can change number of columns
+            cols = 4
+            rows = num_plots // cols if num_plots % cols == 0 else num_plots // cols + 1
+
+            fig = plt.figure(figsize=(cols * 5, rows * 3))
+            gs0 = gridspec.GridSpec(rows, cols, figure=fig)
+            for i, feature in enumerate(cat_features):
+                gs00 = gridspec.GridSpecFromSubplotSpec(5, 1, subplot_spec=gs0[i])
+
+                ax1 = fig.add_subplot(gs00[:-1, :])
+
+                if case == 1:
+                    data_dict = {}
+                    for target_class in list(df[target].unique()):
+                        data_dict[target_class] = []
+                        df_target = df[df[target] == target_class]
+                        for feature_category in list(df_cat[feature].unique()):
+                            df_temp = df_target[df_target[feature] ==
+                                                feature_category]
+                            data_dict[target_class].append(len(df_temp))
+                    plotdata = pd.DataFrame(data_dict,
+                                            index=list(df_cat[feature].unique()))
+                    plotdata.plot(kind="bar", stacked=True, color=pal, ax=ax1)
+                    ax1.set_xlabel(f"Classes in {feature}")
+                    ax1.set_ylabel("frequency")
+                    for tick in ax1.get_xticklabels():
+                        tick.set_rotation(0)
+                    
+
+                else:
+                    sns.boxplot(data=df, x=feature, y=target, ax=ax1)
+                    sns.stripplot(data=df,
+                                x=feature,
+                                y=target,
+                                ax=ax1,
+                                color=".25",
+                                size=2)  #swarmplots
+                    ax1.set_title(f"{target} by {feature}")
+            st.pyplot(fig)
+
+        else:
+            st.write('There are no categorical features in your dataset')    
+
 
 
     def plot_res_corr(df, target): 
-        numerical_features = create_list_numeric_columns(df)
-        n = len(numerical_features)-1
 
-        fig, axes = plt.subplots(nrows=n,
-                                ncols=2,
-                                figsize=(14, 4*n))
-        i = 0
-        for f in numerical_features: 
-            if f  != target:
-                    sns.regplot(data=df, x=f, y=target, color='blue', ax=axes[i, 0])
-                    sns.residplot(data=df, x=f, y=target, color='red', ax = axes[i, 1])
-                    i+=1
 
-        st.pyplot(fig)
+        #condition for categorical features is the same as in plot_categoric_features()
+        num_features = [column for column in df.columns if df[column].dtypes != "object" ] 
+
+        if len(num_features)-1>0:
+
+            # gridspec inside gridspec
+            num_plots = len(num_features)-1
+            #you can change number of columns
+            cols = 4 
+            rows = num_plots//cols if num_plots%cols == 0 else num_plots//cols +1
+
+            fig = plt.figure(figsize = (cols*5,rows*3))
+            gs0 = gridspec.GridSpec(rows, cols, figure=fig)
+            for i,f in enumerate(num_features):
+                gs00 = gridspec.GridSpecFromSubplotSpec(5, 6, subplot_spec=gs0[i])
+
+                ax1 = fig.add_subplot(gs00[0:2, :-1])
+                ax2 = fig.add_subplot(gs00[2:4, :-1])
+
+                sns.regplot(data=df, x=f, y=target, color=pal[1], ax=ax1)
+                sns.residplot(data=df, x=f, y=target, color=pal[3], ax = ax2)
+
+                ax1.set(xlabel='')
+                ax1.tick_params(
+            axis='x',          # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            bottom=False,      # ticks along the bottom edge are off
+            top=False,         # ticks along the top edge are off
+            labelbottom=False) # labels along the bottom edge are off
+
+                #plt.subplots_adjust(wspace=0, hspace=0.1)
+
+            st.pyplot(fig)
+                
+        else:
+            st.write('There are no numerical features in your dataset')
+
+
 
 
     if plot_type == "Numerical":
